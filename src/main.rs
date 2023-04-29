3625e990e0ef976f2809561b8a36c61e496362bd
hazadus <hazadus7@gmail.com>
2023-04-29 11:02:41 +0300

    Extracting the Argument Parser

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("hazagrep running");
     println!("Searching for '{}' in file '{}'", query, file_path);
@@ -14,4 +12,12 @@ fn main() {
         .expect("Should have been able to reaf the file.");
 
     println!("\nFile content:\n\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    // Get parameters from command line.
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
