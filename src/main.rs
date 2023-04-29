557286bd481f46241f3c2652b1bf8a1979d4ea8f
hazadus <hazadus7@gmail.com>
2023-04-29 11:27:55 +0300

    Calling Config::build and Handling Errors

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("hazagrep running");
     println!("Searching for '{}' in file '{}'", config.query, config.file_path);
 
     let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to reaf the file.");
+        .expect("Should have been able to read the file.");
 
     println!("\nFile content:\n\n{contents}");
 }
@@ -20,10 +24,10 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         // Extract parameters from command line.
         if args.len() < 3 {
-            return Err("Not enough arguments!")
+            return Err("not enough arguments!")
         }
 
         let query = args[1].clone();
