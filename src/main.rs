6ea25a5b63f1cac202bbdb948c30a4fe5cd62959
hazadus <hazadus7@gmail.com>
2023-05-07 19:09:47 +0300

    Printing Errors to Standard Error

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use rust_hazagrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -14,7 +14,7 @@ fn main() {
     println!("Searching for '{}' in file '{}'", config.query, config.file_path);
 
     if let Err(e) = rust_hazagrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
